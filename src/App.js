eb0113487373c27a0aaf0f9f0fb62c7020aa04de
vishnugprasad <vishnuanugraha001@gmail.com>
2022-06-06 21:24:32 +0530

    probs

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
-import "./App.css";
-import { useEffect, useState } from "react";
+import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
-  const [timer, setTimer] = useState(0);
-  function stop() {
-    clearInterval(clock);
+
+  const[seconds, setSeconds] = useState(0)
+  const[minutes, setMinutes] = useState(0) 
+  const[start,setStart] = useState(false)
+
+  var timer
+
+  function reset(){
+    setSeconds(0);
+    setMinutes(0);
+    setStart(false)
+  }
+  function startup(){
+    setStart(true)
   }
 
-  function reset() {
-    setTimer(0);
+  useEffect(()=>{
+    if(start === true){ 
+      timer =  setInterval(()=>{
+        setSeconds(seconds+1)
+        if(seconds === 59){
+          setMinutes(minutes+1)
+          setSeconds(0)
+        }
+      }, 1000)
+    }
+
+    return ()=>{clearInterval(timer)}
+
+  })
+
+  
+  function stop(){
+    
+   clearInterval(timer)
+    
   }
-  var clock;
-
-  useEffect(() => {
-    clock = setInterval(() => {
-      setTimer(timer + 1);
-    }, 1000);
-
-    return () => clearInterval(clock);
-  });
+  
   return (
     <div className="App">
-      <h1> TIMER:{timer} </h1>
-      <button onClick={stop}> stop</button>
-      <button onClick={reset}> reset</button>
+      <h1>TIMER</h1>
+      <h1>{minutes<10 ? "0"+minutes: minutes} : {seconds<10 ? "0"+ seconds : seconds}</h1>
+      <button onClick = {reset}>RESET</button>
+      <button onClick = {stop}>STOP</button>
+      <button onClick = {startup}>START</button>
     </div>
   );
-}
+} 
 
 export default App;
